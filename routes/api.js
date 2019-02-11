ae156554581e2920df5724395958f385968996b8
technolog <viktor@allz.su>
2019-02-11 18:13:30 +0300

    api methods: validate & getaddress

---
+++ routes/api.js
@@ -0,0 +1,109 @@
+var express = require('express');
+var router = express.Router();
+const level = require('level');
+const smartholdemApi = require("sthjs-wrapper");
+const jsonFile = require('jsonfile');
+const sth = require("sthjs");
+const bip39 = require("bip39");
+
+const appConfig = jsonFile.readFileSync("./config.json");
+const db = level('.db', {valueEncoding: 'json'});
+
+// 0x - users
+// 1x - address
+
+// main class
+class SHWAY {
+    async init() {
+        smartholdemApi.setPreferredNode(appConfig.smartholdem.preferredNode); // default node
+        smartholdemApi.init('main'); //main or dev
+    }
+
+    async getNewAddressBySalt(account) {
+        let PASS = appConfig.smartholdem.salt + account;
+        let PUB_KEY = await sth.crypto.getKeys(PASS).publicKey;
+        let ADDR = await sth.crypto.getAddress(PUB_KEY);
+
+        await db.put('0x' + account, {
+            "addr": ADDR
+        });
+
+        await db.put('1x' + ADDR, {
+            "account": account,
+            "pubKey": PUB_KEY,
+            "created": Math.floor(Date.now() / 1000)
+        });
+
+        return ({"addr": ADDR});
+    }
+
+    async getAddress(account) {
+        try {
+            let value = await db.get('0x' + account);
+            return (value)
+        } catch (err) {
+            console.log('add new address');
+            return (this.getNewAddressBySalt(account))
+        }
+    }
+
+    async validate(address) {
+        return (sth.crypto.validateAddress(address))
+    }
+
+    async readDb(from, to) {
+        return new Promise((resolve, reject) => {
+            let list = {};
+            db.createReadStream({gte: from + 'x', lt: to + 'x', "limit": 10000})
+                .on('data', function (data) {
+                    list[data.key] = data.value;
+                })
+                .on('error', function (err) {
+                    reject(err);
+                })
+                .on('end', function () {
+                    resolve(list);
+                });
+        });
+    }
+
+}
+
+
+const shWay = new SHWAY();
+shWay.init().then(function () {
+    console.log('GateWay Init');
+});
+
+const dbGetKey = (key) => {
+    return new Promise((resolve, reject) => {
+        db.get(key, function (err, data) {
+            if (!err) {
+                resolve(data);
+            }
+            reject(key);
+        });
+    });
+};
+
+
+/* GET home page. */
+router.get('/getaddress/:account', function (req, res, next) {
+    shWay.getAddress(req.params["account"]).then(function (data) {
+        res.json(data);
+    });
+});
+
+router.get('/validate/:address', function (req, res, next) {
+    shWay.validate(req.params["address"]).then(function (data) {
+        res.json({"valid": data});
+    });
+});
+
+router.get('/dbget/:from/:to', function (req, res, next) {
+    shWay.readDb(req.params["from"], req.params["to"]).then(function (data) {
+        res.json(data);
+    });
+});
+
+module.exports = router;
