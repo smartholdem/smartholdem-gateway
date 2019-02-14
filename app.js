f2e5fbfe92a8f9f02546ce9456045ace6f65a0b1
technolog <viktor@allz.su>
2019-02-14 08:02:30 +0300

    app routes update

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ if (appConfig.app.localhostOnly) {
 }
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 
 var app = express();
@@ -29,7 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
